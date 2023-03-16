669252b7c4f8729f4c9cc561de73538080c2de77
Santi Aguirre <saguirrews@gmail.com>
2023-03-16 13:16:49 -0300

    Added new favicon

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -7,6 +7,7 @@ const Document: React.FC = () => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+        <link rel="shortcut icon" href="/static/favicon.ico" />
       </Head>
       <body className="dark:bg-zinc-800">
         <Main />
